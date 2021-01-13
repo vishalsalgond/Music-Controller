3de2c5abc1b04209c62c5ffa9fd51f50c9b62115
vishalsalgond <vnsalgond@gmail.com>
2021-01-13 21:44:29 +0530

    Second Commit

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import {
@@ -10,21 +10,21 @@ import {
 } from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <p>This is home</p>
-                    </Route>
-                    <Route path="/join" component={RoomJoinPage} />
-                    <Route path="/create" component={CreateRoomPage} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
